4186f59181feedafcd76c84ab42dba56b5e05669
peyman <142385774+py01n@users.noreply.github.com>
2025-09-01 20:24:46 +0330

    create rotors

---
+++ todays_rotor_generator.py
@@ -0,0 +1,17 @@
+import random
+
+alphabet = "abcdefghijklmnopqrctuvwxyz"
+
+# This is rotors
+
+r1 = list(alphabet)
+random.shuffle(r1)
+r1 = "".join(r1)
+
+r2 = list(alphabet)
+random.shuffle(r2)
+r2 = "".join(r2)
+
+r3 = list(alphabet)
+random.shuffle(r3)
+r3 = "".join(r3)
