4f59aef45bd2167cb94eb62645f1e7b3cbafa68d
peyman <142385774+py01n@users.noreply.github.com>
2025-09-01 22:01:48 +0330

    coding enigma

--- todays_rotor_generator.py
+++ todays_rotor_generator.py
@@ -1,6 +1,7 @@
 import random
+import pickle
 
-alphabet = "abcdefghijklmnopqrctuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 # This is rotors
 
@@ -15,3 +16,7 @@ r2 = "".join(r2)
 r3 = list(alphabet)
 random.shuffle(r3)
 r3 = "".join(r3)
+
+f = open("./todays_rotor_state.enigma", "wb")
+pickle.dump((r1, r2, r3), f)
+f.close()
